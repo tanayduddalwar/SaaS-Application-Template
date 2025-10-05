4c3575ef73599d2364e0247fb7b41d8f6669c759
tanayduddalwar <tanay.duddalwar@gmail.com>
2025-10-05 23:34:10 +0530

    Clerk Auth for Google,Github Added

--- api/index.py
+++ api/index.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI  # type: ignore
+from fastapi import FastAPI,Depends # type: ignore
 from fastapi.responses import StreamingResponse  # type: ignore
 from openai import OpenAI  # type: ignore
+from fastapi_clerk_auth import ClerkConfig, ClerkHTTPBearer, HTTPAuthorizationCredentials #type: ignore
 import os
+
 app = FastAPI()
+
+clerk_config = ClerkConfig(jwks_url=os.getenv("CLERK_JWKS_URL"))
+clerk_guard = ClerkHTTPBearer(clerk_config)
+
 google_api_key=os.getenv("GOOGLE_API_KEY")
 @app.get("/api")
-def idea():
+def idea(creds: HTTPAuthorizationCredentials = Depends(clerk_guard)):
+    user_id = creds.decoded["sub"]
     gemini = OpenAI(api_key=google_api_key, base_url="https://generativelanguage.googleapis.com/v1beta/openai/")
     model_name = "gemini-2.0-flash"
     prompt = [{"role": "user", "content": "Reply with a new business idea for AI Agents, formatted with headings, sub-headings and bullet points"}]
